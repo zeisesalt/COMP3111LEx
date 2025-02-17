dc3a5c9b5bc9b13f6d0af282dd15a2fc515b7132
zeisesalt <zeisesalt@sjtu.edu.cn>
2025-02-17 16:42:26 +0800

    tips commit

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -6,7 +6,7 @@ public class mainApp1 {
         int n = 11;
         System.out.println(b + " to power " + n + " = " + myLibrary.Power(b, n));
         System.out.println(n + "! = " + myLibrary.factorial(n));
-        //
+        //another
         System.out.println("Program ..Ended …");
     }
 }
