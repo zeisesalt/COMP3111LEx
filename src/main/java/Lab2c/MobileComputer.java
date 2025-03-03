29242eee7f8db6ca6b01d2f4105d368c212d62cd
zeisesalt <zeisesalt@sjtu.edu.cn>
2025-03-03 15:02:18 +0800

    update lab2

---
+++ src/main/java/Lab2c/MobileComputer.java
@@ -0,0 +1,29 @@
+package Lab2c;
+
+/*	Comp3111LEx\Lab2c\MobileComputer.java
+	Inherits from Computer, class library for Lab2 Exercise 3	*/
+
+public class MobileComputer extends Computer implements Chargeable{
+    private int battery;
+    public MobileComputer() {
+        secret = "MobileComputer secret";
+        battery = 5;
+    }
+    @Override
+    public void work() {
+        if (battery > 0) {
+            System.out.println("It is working on my lap.");
+            battery--;
+        } else
+            System.out.println("Running out of battery");
+    }
+
+    public void charge() {
+        if (battery < 10){
+            battery++;
+            System.out.println("MobileComputer is charging.");
+        }else{
+            System.out.println("MobileComputer charged");
+        }
+    }
+}
